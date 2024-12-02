eb726c9f4318470e17d69749ac005c0bf6bc6e9e
ronnilenvighansen <ronnilenvighansen@gmail.com>
2024-12-02 17:28:44 +0000

    More testsss

--- PatientService.Tests/PatientControllerTests.cs
+++ PatientService.Tests/PatientControllerTests.cs
@@ -37,8 +37,7 @@ public class PatientControllerTests
         // Assert
         var actionResult = Assert.IsType<ActionResult<IEnumerable<Patient>>>(result);
 
-        // Verify the inner OkObjectResult
-        var okResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+        var okResult = Assert.IsType<ObjectResult>(actionResult.Result);
         var returnValue = Assert.IsAssignableFrom<IEnumerable<Patient>>(okResult.Value);
         Assert.Equal(mockPatients.Count(), returnValue.Count());
     }
